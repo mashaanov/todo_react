e0a14376457708302e0b4e837fd683e4db4f470c
Maria Novikova <m.v.novikovaaa@gmail.com>
2024-11-10 19:14:09 +0400

    improve styles Item.jsx

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -4,22 +4,26 @@ export default class Item extends React.Component {
   render() {
     const { task, onRemove, onToggleState, isCompleted } = this.props;
     return (
-      <div className="row">
+      <div className="row align-items-center">
         <div className="col-auto">
           <button
             type="button"
             className="btn btn-primary btn-sm"
             onClick={onRemove}
+            aria-label="Удалить задачу"
           >
-            -
+            &minus;
           </button>
         </div>
         <div
-          className="col"
+          className={`col w-50 ${isCompleted ? "text-muted" : ""}`}
           onClick={onToggleState}
-          style={{ cursor: "pointer" }}
+          style={{
+            cursor: "pointer",
+            textDecoration: isCompleted ? "line-through" : "none",
+          }}
         >
-        {isCompleted ? <s>{task}</s> : task}
+          {task}
         </div>
       </div>
     );
